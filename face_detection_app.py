22063875aa8d388081e9f099499730d484b1e2e4
Yaqoob David <yaqoob.david@wowtech.com>
2023-04-28 12:51:46 +0200

    adding commit to python file

--- face_detection_app.py
+++ face_detection_app.py
@@ -1,9 +1,14 @@
+# import necessary libraries
 from flask import Flask,render_template,Response
 import cv2
 
+# create a Flask app instance
 app=Flask(__name__)
+
+# set up the camera as the video source
 camera=cv2.VideoCapture(0)
 
+# define a function to generate video frames
 def gen_frames():
     while True:
             
@@ -23,7 +28,7 @@ def gen_frames():
             for (x,y, w, h ) in faces:
                 cv2.rectangle(frame,(x,y),(x+w, y+h),(255,0,0),2) 
                 
-                
+            # convert the frame to a JPEG image and yield it to the browser
                 roi_gray = gray [y:y+h, x:x+w]
                 roi_color = frame [y:y+h, x:x+w]
                 eyes = eye_cascade.detectMultiScale(roi_gray,1.1,7)
@@ -34,13 +39,25 @@ def gen_frames():
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+            
+            
+# define the root URL endpoint
 @app.route('/')
 def index():
+    
+    # render the index.html template
     return render_template('index.html')
+
+# define a video feed URL endpoint
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+    # return the video stream as a Flask response
+    return Response(gen_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+    
+ 
+# Start the Flask app with debugging enabled    
 if __name__=='__main__':
     app.run(debug=True)        
                     
